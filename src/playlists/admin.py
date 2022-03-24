b595a56189f1e0e44d21c62454a686997ef652c5
Artus U <artusrock@hotmail.com>
2022-03-24 10:27:34 +0000

    Playlist Type Choices Fixes #15

--- src/playlists/admin.py
+++ src/playlists/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import TVShowProxy, TVShowSeasonProxy, Playlist, PlaylistItem
+from .models import MovieProxy, TVShowProxy, TVShowSeasonProxy, Playlist, PlaylistItem
+
+
+class MovieProxyAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    fields = ['title', 'description', 'state', 'video', 'slug']
+    class Meta:
+        model = MovieProxy
+
+    def get_queryset(self, request):
+        return MovieProxy.objects.all()
+
+admin.site.register(MovieProxy, MovieProxyAdmin)
 
 
 class SeasonEpisodeInline(admin.TabularInline):
@@ -46,5 +58,8 @@ class PlaylistAdmin(admin.ModelAdmin):
     inlines = [PlaylistItemInline]
     class Meta:
         model = Playlist
+        
+    def get_queryset(self, request):
+        return Playlist.objects.filter(type=Playlist.PlaylistTypeChoices.PLAYLIST)
 
 admin.site.register(Playlist, PlaylistAdmin)
