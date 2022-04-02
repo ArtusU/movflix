a3978933b50db4994463c98224ffbe6eafcbb1eb
Artus U <artusrock@hotmail.com>
2022-04-02 12:00:22 +0100

    Detail Views Fixes #31

--- src/movflix/urls.py
+++ src/movflix/urls.py
@@ -1,12 +1,36 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from playlists.views import MovieListView, TVShowListView, FeaturedPlaylistListView
+from playlists.views import (
+    MovieListView, 
+    TVShowListView, 
+    FeaturedPlaylistListView, 
+    MovieDetailView, 
+    PlaylistDetailView,
+    TVShowSeasonDetailView,
+    TVShowDetailView
+    )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('', FeaturedPlaylistListView.as_view()),
     path('movies/', MovieListView.as_view()),
+    path('movies/<slug:slug>/', MovieDetailView.as_view()),
+    path('movies/', MovieListView.as_view()),
+    
+    path('media/<int:pk>/', PlaylistDetailView.as_view()),
+    
+    # path('category/', include('categories.urls')),
+    # path('categories/', include('categories.urls')),
+    
+    # path('search/', SearchView.as_view()),
+    
+    path('shows/<slug:showSlug>/seasons/<slug:seasonSlug>/', TVShowSeasonDetailView.as_view()),
+    path('shows/<slug:slug>/seasons/', TVShowDetailView.as_view()),
+    # path('shows/<slug:slug>/', TVShowDetailView.as_view()),
     path('shows/', TVShowListView.as_view()),
+    
+    # path('tags/', include('tags.urls')),
+    # path('object-rate/', rate_object_view)
 ]
--- src/playlists/views.py
+++ src/playlists/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.http import Http404
+from django.views.generic import ListView, DetailView
 
-from .models import Playlist, MovieProxy, TVShowProxy
+from django.utils import timezone
+
+from movflix.db.models import PublishStateOptions
+
+from .models import Playlist, MovieProxy, TVShowProxy, TVShowSeasonProxy
 
 
 
@@ -21,10 +25,55 @@ class PlaylistMixin():
 class MovieListView(PlaylistMixin, ListView):
     queryset = MovieProxy.objects.all()
     title = "Movies"
+    
+    
+class MovieDetailView(PlaylistMixin, DetailView):
+    template_name = 'playlists/movie_detail.html'
+    queryset = MovieProxy.objects.all()
+
+
+class PlaylistDetailView(PlaylistMixin, DetailView):
+    template_name = 'playlists/playlist_detail.html'
+    queryset = Playlist.objects.all()
+
 
 class TVShowListView(PlaylistMixin, ListView):
     queryset = TVShowProxy.objects.all()
     title = "TV Shows"
+    
+    
+class TVShowDetailView(PlaylistMixin, DetailView):
+    template_name = 'playlists/tvshow_detail.html'
+    queryset = TVShowProxy.objects.all()
+
+
+class TVShowSeasonDetailView(PlaylistMixin, DetailView):
+    template_name = 'playlists/season_detail.html'
+    queryset = TVShowSeasonProxy.objects.all()
+
+    def get_object(self):
+        kwargs = self.kwargs
+        show_slug = kwargs.get("showSlug")
+        season_slug = kwargs.get("seasonSlug")
+        now = timezone.now()
+        try:
+            obj = TVShowSeasonProxy.objects.get(
+                state=PublishStateOptions.PUBLISH,
+                publish_timestamp__lte=now,
+                parent__slug__iexact=show_slug,
+                slug__iexact=season_slug
+            )
+        except TVShowSeasonProxy.MultipleObjectsReturned:
+            qs = TVShowSeasonProxy.objects.filter(
+                parent__slug__iexact=show_slug,
+                slug__iexact=season_slug
+            ).published()
+            obj = qs.first()
+            # log this
+        except:
+            raise Http404
+        return obj
+
 
 class FeaturedPlaylistListView(PlaylistMixin, ListView):
     queryset = Playlist.objects.featured_playlists()
